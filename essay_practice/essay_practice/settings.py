3a48f25802e698f334a0e6a40075bbff7003b8f4
Evgeniy Perov <evgeniy.perov.05@mail.ru>
2022-12-10 19:26:17 +0500

    env config

--- essay_practice/essay_practice/settings.py
+++ essay_practice/essay_practice/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!s@k9lt2$zedbjwb@2hlx+ld_8a9m7v($bl29+j=vy0t2bbh)r'
+SECRET_KEY = config('SECRET_KEY', default='NO-SO-SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool, default=True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1').split()
 
 
 # Application definition
