41aa1cc8f7f61d850f857a0e2445dc747079c870
nadia <crocodile.who.wears.socks@yandex.ru>
2022-12-13 18:19:12 +0300

    fixed

--- essay_practice/grades/models.py
+++ essay_practice/grades/models.py
@@ -5,27 +5,33 @@ from writing.models import Essay
 
 
 class Essay_Grade(models.Model):
-    reviewer = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    reviewer = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        verbose_name='оценено пользователем')
+
     essay = models.ForeignKey(
         Essay,
         on_delete=models.CASCADE,
-        verbose_name='Сочинение')
+        verbose_name='сочинение')
+
+    GRADE_CHOICES = (0, '0'), (1, '1'), (2, '2')
 
     relevance_to_topic = models.IntegerField(
-        choices=((0, '0'), (1, '1'), (2, '2')),
-        verbose_name='Соответствие теме')
+        choices=GRADE_CHOICES,
+        verbose_name='соответствие теме')
 
     matching_args = models.IntegerField(
-        choices=((0, '0'), (1, '1'), (2, '2')),
-        verbose_name='Аргументация')
+        choices=GRADE_CHOICES,
+        verbose_name='аргументация')
 
     composition = models.IntegerField(
-        choices=((0, '0'), (1, '1'), (2, '2')),
-        verbose_name='Композиция')
+        choices=GRADE_CHOICES,
+        verbose_name='композиция')
 
     speech_quality = models.IntegerField(
-        choices=((0, '0'), (1, '1'), (2, '2')),
-        verbose_name='Качество речи')
+        choices=GRADE_CHOICES,
+        verbose_name='качество речи')
 
     comment = models.TextField()
     pub_date = models.DateTimeField(auto_now=True)
