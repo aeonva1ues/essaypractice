82b9d48c950c9b1ba4bfff5636d6ebf4831d7755
nadia <crocodile.who.wears.socks@yandex.ru>
2022-12-13 16:51:39 +0300

    essay and essay_grade models added

--- essay_practice/grades/models.py
+++ essay_practice/grades/models.py
@@ -1,1 +1,35 @@
-# from django.db import models
+from django.db import models
+
+from users.models import Profile
+from writing.models import Essay
+
+
+class Essay_Grade(models.Model):
+    reviewer = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    essay = models.ForeignKey(
+        Essay,
+        on_delete=models.CASCADE,
+        verbose_name='Сочинение')
+
+    relevance_to_topic = models.IntegerField(
+        choices=((0, '0'), (1, '1'), (2, '2')),
+        verbose_name='Соответствие теме')
+
+    matching_args = models.IntegerField(
+        choices=((0, '0'), (1, '1'), (2, '2')),
+        verbose_name='Аргументация')
+
+    composition = models.IntegerField(
+        choices=((0, '0'), (1, '1'), (2, '2')),
+        verbose_name='Композиция')
+
+    speech_quality = models.IntegerField(
+        choices=((0, '0'), (1, '1'), (2, '2')),
+        verbose_name='Качество речи')
+
+    comment = models.TextField()
+    pub_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'оценка'
+        verbose_name_plural = 'оценки'
--- essay_practice/writing/models.py
+++ essay_practice/writing/models.py
@@ -1,3 +1,42 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+from users.models import Profile
+
+
+class Section(models.Model):
+    name = models.CharField(max_length=150)
+
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=150)
+    section = models.ForeignKey(Section, on_delete=models.CASCADE,
+                                help_text='Раздел')
+
+    class Meta:
+        verbose_name = 'тема'
+        verbose_name_plural = 'темы'
+
+
+class Essay(models.Model):
+    author = models.ForeignKey(Profile, on_delete=models.SET_DEFAULT,
+                               default='автор неизвестен')
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    intro = models.TextField(help_text='Вступление')
+    first_arg = models.TextField(help_text='1-ый аргумент')
+    second_arg = models.TextField(help_text='2-ой аргумент')
+    closing = models.TextField(help_text='Заключение')
+
+    grades = models.ManyToManyField(
+        Profile,
+        through='grades.essay_grade',
+        related_name='essaygrade'
+        )
+    pub_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'сочинение'
+        verbose_name_plural = 'сочинения'
