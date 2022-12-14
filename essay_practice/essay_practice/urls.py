4a7012257870a8112684fdd09235951c22859ed8
Evgeniy Perov <evgeniy.perov.05@mail.ru>
2022-12-14 20:41:14 +0500

    register media dir and add icons

--- essay_practice/essay_practice/urls.py
+++ essay_practice/essay_practice/urls.py
@@ -1,6 +1,10 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +12,12 @@ urlpatterns = [
     path('auth/', include('users.urls')),
     path('auth/', include('django.contrib.auth.urls')),
 ]
+
+
+if settings.DEBUG:
+    if settings.MEDIA_ROOT:
+        urlpatterns += static(
+            settings.MEDIA_URL,
+            document_root=settings.MEDIA_ROOT
+        )
+    urlpatterns += staticfiles_urlpatterns()
