3a854575f5fa8e9aac0bddf9b2ca4118928ab8a0
Evgeniy Perov <evgeniy.perov.05@mail.ru>
2022-12-15 18:02:29 +0500

    part with bug

--- essay_practice/writing/forms.py
+++ essay_practice/writing/forms.py
@@ -1,11 +1,20 @@
+from django.shortcuts import get_object_or_404
 from django import forms
-from .models import Essay, Topic
+from writing.models import Essay, Topic, Section
 
 
 class WritingEssayForm(forms.ModelForm):
+    section = forms.IntegerField(
+        required=False,
+        widget=forms.HiddenInput(),
+        label=''
+    )
+
     def __init__(self, *args, **kwargs):
         super(WritingEssayForm, self).__init__(*args, **kwargs)
-        self.fields['topic'].queryset = Topic.objects.order_by('?')[:3]
+        section = get_object_or_404(Section, id=kwargs['initial']['section'])
+        topics = Topic.objects.filter(section=section).order_by('?')[:3]
+        self.fields['topic'].queryset = topics
 
     class Meta:
         model = Essay
