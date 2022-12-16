2dd3147021faa4b4633dc7aa3f629e942f2b5117
Evgeniy Perov <evgeniy.perov.05@mail.ru>
2022-12-16 17:08:54 +0500

    pep8 fix

--- essay_practice/grades/admin.py
+++ essay_practice/grades/admin.py
@@ -4,4 +4,7 @@ from .models import Essay_Grade
 
 @admin.register(Essay_Grade)
 class EssayGradeAdmin(admin.ModelAdmin):
-    list_display = ('reviewer', 'essay', 'relevance_to_topic', 'matching_args', 'composition', 'speech_quality', 'comment', 'pub_date')
+    list_display = (
+        'reviewer', 'essay', 'relevance_to_topic',
+        'matching_args', 'composition', 'speech_quality',
+        'comment', 'pub_date')
