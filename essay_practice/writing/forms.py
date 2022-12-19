53c0c7369167eda37abecc50322569a892dc7512
Evgeniy Perov <evgeniy.perov.05@mail.ru>
2022-12-20 01:03:12 +0500

    kd feature

--- essay_practice/writing/forms.py
+++ essay_practice/writing/forms.py
@@ -4,7 +4,13 @@ from django import forms
 from writing.models import Essay, Topic, Section
 
 
+from django.utils import timezone
+
 class WritingEssayForm(forms.ModelForm):
+    class Meta:
+        model = Essay
+        fields = ('topic', 'intro', 'first_arg', 'second_arg', 'closing')
+
     section = forms.IntegerField(
         required=False,
         widget=forms.HiddenInput(),
@@ -17,7 +23,8 @@ class WritingEssayForm(forms.ModelForm):
             field.field.widget.attrs['class'] = 'form-control'
             field.field.widget.attrs['style'] = 'border-left-color: black'
             field.field.widget.attrs['placeholder'] = field.field.label
-        section = get_object_or_404(Section, id=kwargs['initial']['section'])
+        self.section_pk = kwargs['initial']['section']
+        section = get_object_or_404(Section, id=self.section_pk)
         topics = Topic.objects.filter(section=section).order_by('?')
         self.fields['topic'].queryset = topics
 
@@ -33,6 +40,10 @@ class WritingEssayForm(forms.ModelForm):
         ) < 250:
             raise ValidationError('Сочинение не прошло по объему.')
 
-    class Meta:
-        model = Essay
-        fields = ('topic', 'intro', 'first_arg', 'second_arg', 'closing')
+        last = self._meta.model.objects.order_by(
+            'pub_date').only('pub_date').exclude(pk=self.section_pk).last()
+        if last:
+            if last.pub_date - timezone.now() < timezone.timedelta(minutes=20):
+                raise ValidationError(
+                    'Вы подозрительно быстро пишете сочинения!')
+        self.pub_date = timezone.now()
