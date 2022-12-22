a2a7708ac70d1abac0010ed63b2ba9e5cc1a1bfd
Evgeniy Perov <evgeniy.perov.05@mail.ru>
2022-12-22 18:48:50 +0500

    report from feed feauture

---
+++ essay_practice/essayfeed/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from essayfeed.models import Essay_Report
+
+
+class ReportEssayForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ReportEssayForm, self).__init__(*args, **kwargs)
+        for field in self.visible_fields():
+            field.field.widget.attrs['class'] = 'form-control'
+
+    class Meta:
+        model = Essay_Report
+        fields = ('reason',)
