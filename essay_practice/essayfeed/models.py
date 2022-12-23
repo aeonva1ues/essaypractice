e8abba15a5aa9a83001e5c724667f4d675cb68a1
nadia <crocodile.who.wears.socks@yandex.ru>
2022-12-23 21:58:40 +0300

    commentreport model created

--- essay_practice/essayfeed/models.py
+++ essay_practice/essayfeed/models.py
@@ -2,17 +2,20 @@ from django.db import models
 
 from users.models import Profile
 from writing.models import Essay
+from grades.models import Essay_Grade
+
+
+REASON_CHOICES = (
+        (0, 'Оффтоп/Спам'),
+        (1, 'Пропаганда/Реклама'),
+        (2, 'Нецензурная лексика')
+    )
 
 
 class Essay_Report(models.Model):
     '''
     Жалобы на сочинение, оставленные пользователями
     '''
-    REASON_CHOICES = (
-        (0, 'Оффтоп/Спам'),
-        (1, 'Пропаганда/Реклама'),
-        (2, 'Нецензурная лексика')
-    )
     from_user = models.ForeignKey(
         Profile,
         on_delete=models.CASCADE,
@@ -42,3 +45,34 @@ class Essay_Report(models.Model):
 
     def __str__(self):
         return f'Жалоба #{self.id}'
+
+
+class CommentReport(models.Model):
+    '''
+    Жалоба на комментарий под сочинением
+    '''
+    from_user = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        verbose_name='автор жалобы'
+    )
+    comment = models.ForeignKey(
+        Essay_Grade,
+        on_delete=models.CASCADE,
+        verbose_name='комментарий с нарушением'
+    )
+    reason = models.IntegerField(
+        choices=REASON_CHOICES,
+        verbose_name='нарушение'
+    )
+    report_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='дата жалобы'
+    )
+
+    class Meta:
+        verbose_name = 'жалоба на комментарий'
+        verbose_name_plural = 'жалобы на комментарии'
+
+    def __str__(self):
+        return f'Жалоба #{self.id} на комментарий'
