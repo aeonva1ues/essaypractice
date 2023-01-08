1083599b1ec4fbb733d8704af47c93004d8d33e9
Evgeniy Perov <evgeniy.perov.05@mail.ru>
2023-01-09 00:43:08 +0500

    fix error when pwd1 not equal pwd2

--- essay_practice/users/forms.py
+++ essay_practice/users/forms.py
@@ -28,15 +28,16 @@ class SignUpForm(UserCreationForm):
         }
 
     def clean(self):
-        password1 = self.cleaned_data['password1']
-        password2 = self.cleaned_data['password2']
         email = self.cleaned_data['email']
         username = self.cleaned_data['username']
         if Profile.objects.filter(email=email).count() > 0:
             raise ValidationError('Аккаунт с такой почтой уже существует!')
         if Profile.objects.filter(username=username).count() > 0:
             raise ValidationError('Аккаунт с таким логином уже существует!')
-        if password1 and password2 and password1 != password2:
+        if ('password1' in self.cleaned_data and
+                'password2' in self.cleaned_data and
+                self.cleaned_data['password1'] != self.cleaned_data[
+                    'password2']):
             raise ValidationError(
                 'Пароли не совпали!'
             )
